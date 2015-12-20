31e29602160c79022dccc37c0b5f3c34c93ee05d
Dawn Vana <dawn.vana@gmail.com>
2015-12-20 15:59:41 -0600

    added create action to controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,19 @@
 class ProfilesController < ApplicationController
+  def create
+    @profile = Unirest.post(
+      "http://localhost:3000/profiles/.json",
+      headers: { "Accept" => "profile/json" },
+      parameters: {
+        name: params[:name]
+        description: params[:description]
+        url: params[:url]
+        screenshot_url: params[:screenshot_url]
+        student_id: params[:student_id]
+      }
+    ).body
+    redirect_to "/profiles/#{@profile['id]'  
+  end
+
   def edit
     @profile = Unirest.get("http://localhost:3000/profiles/#{params[:id]}.json").body 
   end
@@ -6,7 +21,7 @@ class ProfilesController < ApplicationController
   def update
     @profile = Unirest.patch(
       "http://localhost:3000/profiles/#{params[:id]}.json",
-      headers: {"Accept" => 'application/json'},
+      headers: {"Accept" => "application/json"},
       parmeters: {
         name: params[:name],
         description: params[:description],
