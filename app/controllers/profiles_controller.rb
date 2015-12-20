faebf1bf06e7f2d1edcdb772c4aef19834cdd53f
Dawn Vana <dawn.vana@gmail.com>
2015-12-20 15:04:47 -0600

    added edit and update in controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,20 @@
 class ProfilesController < ApplicationController
+  def edit
+    @profile = Unirest.get("http://localhost:3000/profiles/#{params[:id]}.json").body 
+  end
+
+  def update
+    @profile = Unirest.patch(
+      "http://localhost:3000/profiles/#{params[:id]}.json",
+      headers: {"Accept" => 'application/json'},
+      parmeters: {
+        name: params[:name],
+        description: params[:description],
+        url: params[:url],
+        screenshot_url: params[:screenshot_url],
+        student_id: params[:student_id]
+        }
+    ).body
+    redirect_to "/profiles/#{@profile['id']}"
+  end
 end
