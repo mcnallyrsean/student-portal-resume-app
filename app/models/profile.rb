72c1576b0ae3f1bf19c735bf77a1e0aff6f90fd0
fathergoose <al@ilseman.com>
2015-12-21 19:41:27 -0600

    poro is made

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,17 @@
 class Student
   attr_accessor :first_name, :last_name, :email, :phone, :short_bio, :twitter, :blog_url, :resume_url, :github_url, :photo_url, :user_id, :educations, :experiences, :capstones, :skills
   def initialize(p)
+    @first_name = p['first_name']
+    @last_name = p['last_name']
+    @email = p['email']
+    @phone = p['phone']
+    @short_bio = p['short_bio']
+    @twitter = p['twitter']
+    @blog_url = p['blog_url']
+    @resume_url = p['resume_url']
+    @github_url = p['github_url']
+    @photo_url = p['photo_url']
+    @user_id = p['user_id']
     @capstones = Capstone.all
     @educations = Education.all
     @experiences = Experience.all
