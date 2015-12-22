cca23f44e5100da9892678627885f4f83009c2e2
jhsiaomei <jw1314@gmail.com>
2015-12-21 20:02:45 -0600

    added users and sessions controllers, routes, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/profiles/new' => 'profiles#new'
   post '/profiles' => 'profiles#create'
   get '/profiles/:id' => 'profiles#show'
   get '/profiles/:id/edit' => 'profiles#edit'
   patch '/profiles/:id' => 'profiles#update'
+
+  get '/sign_in' => 'sessions#new'
+  post '/sign_in' => 'sessions#create'
+  delete '/sign_out' => 'sessions#destroy'
 end
